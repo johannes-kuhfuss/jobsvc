4578f7a1c7aa88bcca5650f4c18f6bb5639b8ae1
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-12-18 16:40:40 +0100

    wip infrastructure

--- app/application.go
+++ app/application.go
@@ -1,11 +1,43 @@
 package app
 
 import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/johannes-kuhfuss/jobsvc/config"
 	"github.com/johannes-kuhfuss/services_utils/logger"
+)
+
+var (
+	router *gin.Engine
 )
 
 func StartApp() {
 	logger.Info("Starting application")
+	err := config.InitConfig(config.EnvFile)
+	if err != nil {
+		panic(err)
+	}
+	initRouter()
 	mapUrls()
+	startRouter()
 	logger.Info("Application ended")
+}
+
+func initRouter() {
+	gin.SetMode(config.Cfg.Gin.Mode)
+	gin.DefaultWriter = logger.GetLogger()
+	router = gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.SetTrustedProxies(nil)
+}
+
+func startRouter() {
+	listenAddr := fmt.Sprintf("%s:%s", config.Cfg.Server.Host, config.Cfg.Server.Port)
+	logger.Info(fmt.Sprintf("Listening on %v", listenAddr))
+	if err := router.Run(listenAddr); err != nil {
+		logger.Error("Error while starting router", err)
+		panic(err)
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,50 @@
 package config
+
+import (
+	"github.com/johannes-kuhfuss/services_utils/api_error"
+	"github.com/johannes-kuhfuss/services_utils/logger"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Server struct {
+		Host string `envconfig:"SERVER_HOST"`
+		Port string `envconfig:"SERVER_PORT" default:"8080"`
+	}
+	Gin struct {
+		Mode string `envconfig:"GIN_MODE" default:"release"`
+	}
+	Database struct {
+		Username string `envconfig:"DB_USERNAME" required:"true"`
+		Password string `envconfig:"DB_PASSWORD" required:"true"`
+	}
+}
+
+const (
+	EnvFile = ".env"
+)
+
+var (
+	Cfg Config
+)
+
+func InitConfig(file string) api_error.ApiErr {
+	logger.Info("Initalizing configuration")
+	loadConfig(file)
+	err := envconfig.Process("", &Cfg)
+	if err != nil {
+		return api_error.NewInternalServerError("Could not configure app, Check your environment variables", err)
+	}
+	logger.Info("Done initalizing configuration")
+	return nil
+}
+
+func loadConfig(file string) error {
+	err := godotenv.Load(file)
+	if err != nil {
+		logger.Error("Could not open env file", err)
+		return err
+	}
+	return nil
+}
