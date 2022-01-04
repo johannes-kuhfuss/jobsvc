dcb995fd1b38997d054a3be8ce8f01d5fe410506
johannes-kuhfuss <johannes.kuhfuss@web.de>
2022-01-04 08:26:58 +0100

    WIP Unit Tests for JobRespository

--- repositories/JobRepositoryDb_test.go
+++ repositories/JobRepositoryDb_test.go
@@ -1,11 +1,15 @@
 package repositories
 
 import (
+	"database/sql"
+	"net/http"
+	"regexp"
 	"testing"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/johannes-kuhfuss/jobsvc/config"
 	"github.com/johannes-kuhfuss/services_utils/logger"
+	"github.com/stretchr/testify/assert"
 	sqlmock "github.com/zhashkevych/go-sqlxmock"
 )
 
@@ -34,4 +38,45 @@ func setupTest(t *testing.T) func() {
 func Test_FindAll_NoStatus_Returns_DbError(t *testing.T) {
 	teardown := setupTest(t)
 	defer teardown()
+
+	sqlErr := sql.ErrConnDone
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM joblist")).WillReturnError(sqlErr)
+
+	jobs, err := jrd.FindAll("")
+
+	assert.Nil(t, jobs)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode())
+	assert.EqualValues(t, "Database error finding all jobs", err.Message())
 }
+
+func Test_FindAll_NoStatusNoResults_Returns_NotFoundError(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+
+	rows := sqlmock.NewRows([]string{})
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM joblist")).WillReturnRows(rows)
+
+	jobs, err := jrd.FindAll("")
+
+	assert.Nil(t, jobs)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode())
+	assert.EqualValues(t, "No jobs found", err.Message())
+}
+
+/*
+func Test_FindAll_WithStatusNoResults_Returns_Results(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+
+	// define result set
+	rows := sqlmock.NewRows([]string{})
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM joblist")).WillReturnRows(rows)
+
+	jobs, err := jrd.FindAll("")
+
+	assert.NotNil(t, jobs)
+	assert.Nil(t, err)
+}
+*/
