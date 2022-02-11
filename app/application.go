8a410ce2a3be9cfa6bab180cd5d86277d68836f9
johannes-kuhfuss <johannes.kuhfuss@web.de>
2022-02-11 09:05:07 +0100

    Group routes

--- app/application.go
+++ app/application.go
@@ -136,20 +136,25 @@ func wireApp() {
 }
 
 func mapUrls() {
-	// API
-	cfg.RunTime.Router.POST("/jobs", jobHandler.CreateJob)
-	cfg.RunTime.Router.GET("/jobs", jobHandler.GetAllJobs)
-	cfg.RunTime.Router.GET("/jobs/:job_id", jobHandler.GetJobById)
-	cfg.RunTime.Router.DELETE("/jobs/:job_id", jobHandler.DeleteJobById)
-	cfg.RunTime.Router.DELETE("/jobs", jobHandler.DeleteAllJobs)
-	cfg.RunTime.Router.PUT("/jobs/:job_id", jobHandler.UpdateJob)
-	cfg.RunTime.Router.PUT("/jobs/:job_id/status", jobHandler.SetStatusById)
-	cfg.RunTime.Router.PUT("/jobs/:job_id/history", jobHandler.SetHistoryById)
-	cfg.RunTime.Router.PUT("/jobs/dequeue", jobHandler.Dequeue)
-	// UI
-	cfg.RunTime.Router.GET("/", jobUiHandler.JobListPage)
-	cfg.RunTime.Router.GET("/config", jobUiHandler.ConfigPage)
-	cfg.RunTime.Router.GET("/about", jobUiHandler.AboutPage)
+	api := cfg.RunTime.Router.Group("/jobs")
+	{
+		api.POST("/", jobHandler.CreateJob)
+		api.GET("/", jobHandler.GetAllJobs)
+		api.GET("/:job_id", jobHandler.GetJobById)
+		api.DELETE("/:job_id", jobHandler.DeleteJobById)
+		api.DELETE("/", jobHandler.DeleteAllJobs)
+		api.PUT("/:job_id", jobHandler.UpdateJob)
+		api.PUT("/:job_id/status", jobHandler.SetStatusById)
+		api.PUT("/:job_id/history", jobHandler.SetHistoryById)
+		api.PUT("/dequeue", jobHandler.Dequeue)
+
+	}
+	ui := cfg.RunTime.Router.Group("/")
+	{
+		ui.GET("/", jobUiHandler.JobListPage)
+		ui.GET("/config", jobUiHandler.ConfigPage)
+		ui.GET("/about", jobUiHandler.AboutPage)
+	}
 }
 
 func RegisterForOsSignals() {
